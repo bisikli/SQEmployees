aa202eb5eb44f6ad06fc67a068bbc51c9a48e464
Bilgehan <bilgehan.isikli@gmail.com>
2019-11-12 12:28:15 -0800

    Initial Commit

---
+++ SquareEmployees/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SquareEmployees
+//
+//  Created by Bilgehan Işıklı on 12.11.2019.
+//  Copyright © 2019 Bilgehan Işıklı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
